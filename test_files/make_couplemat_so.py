6553b3e1b34687ca9b8e3c2e8651189c4a2bad86
Erik Rosenberg <errosenberg47@gmail.com>
2024-02-28 17:03:48 +0000

    minor refactor of make_couplemat_so

--- test_files/make_couplemat_so.py
+++ test_files/make_couplemat_so.py
@@ -7,19 +7,20 @@ params_data = szifi.params_data_so
 params_model = szifi.params_model_default
 bin_fac = 4
 lmax1d = 10000
-fields = [208, 209]
+#fields = np.loadtxt('/nvme1/scratch/erosen/data/so_sims/cmb+noise+sz_car_f32/healpix_ids_sosimmask_nside008.txt').astype('int64')
+fields = np.arange(272, 744)
 params_data['powspec_lmax1d'] = lmax1d
 params_data['powspec_bin_fac'] = bin_fac
-params_data['field_ids'] = fields
-data = szifi.input_data(params_szifi=params_szifi,params_data=params_data)
 path = params_szifi["path"]
 
 for field_id in fields:
+    params_data['field_ids'] = [field_id]
+    data = szifi.input_data(params_szifi=params_szifi,params_data=params_data)
 
     mask = data.data["mask_ps"][field_id]
     pix = data.data["pix"][field_id]
     
     new_shape = szifi.maps.get_newshape_lmax1d((pix.nx, pix.nx), lmax1d, pix.dx, False)
-    print(new_shape)
-    ps = szifi.power_spectrum(pix,mask=mask,cm_compute=True,cm_compute_scratch=True,
+    print(field_id, new_shape)
+    ps = szifi.power_spectrum(pix,mask=mask,cm_compute=True,cm_compute_scratch=False,
                               bin_fac=bin_fac, new_shape=new_shape, cm_save=True,cm_name=data.data['coupling_matrix_name'][field_id])
