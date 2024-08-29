09f04c5e98900f09431dd484f06e14487b59e214
Erik Rosenberg <errosenberg47@gmail.com>
2024-08-29 14:31:41 +0100

    Fix old names in utils.py

--- szifi/utils.py
+++ szifi/utils.py
@@ -12,7 +12,7 @@ def get_q_bias(pix,tem,signal,cov_mf_true,cov_noi_true,n_modes_per_bin_map,type=
     tem_fft = maps.get_fft(tem,pix)
     signal_fft = maps.get_fft(signal,pix)
 
-    n0 = filter_sum_1(tem_fft,tem_fft,cov_mf_true)
+    n0 = filter_sum(tem_fft,tem_fft,cov_mf_true)
 
     map1 = (signal_fft+np.conjugate(signal_fft))*cov_noi_true*np.conjugate(tem_fft)
     map2 = (signal_fft+np.conjugate(signal_fft))*cov_noi_true*np.conjugate(tem_fft)*np.abs(tem_fft)**2
@@ -40,9 +40,9 @@ def get_q_std_bias(pix,tem,signal,cov_mf_true,cov_noi_true):
 
     tem_fft = maps.get_fft(tem,pix)
     signal_fft = maps.get_fft(signal,pix)
-    n0 = filter_sum_1(tem_fft,tem_fft,cov_mf_true)
-    n_bias = filter_sum_1(tem_fft,tem_fft*cov_noi_true,(cov_mf_true)**2)
-    n_bias = filter_sum_1(tem_fft,tem_fft*cov_noi_true,(cov_mf_true)**2)
+    n0 = filter_sum(tem_fft,tem_fft,cov_mf_true)
+    n_bias = filter_sum(tem_fft,tem_fft*cov_noi_true,(cov_mf_true)**2)
+    n_bias = filter_sum(tem_fft,tem_fft*cov_noi_true,(cov_mf_true)**2)
     sigma_unbiased = 1./np.sqrt(n0)
     sigma_biased = 1./np.sqrt(n_bias)
 
